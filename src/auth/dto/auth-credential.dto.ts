b3f2d0c60d39f0a9e7420afb1fefc813c0ea1436
Namik Hwang <nhwang@c8r1s2.42seoul.kr>
2023-06-12 13:00:51 +0900

    validation add for user

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    //eng, num
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: "password only accecpt eng and number"
+    })
     password: string;
 }
