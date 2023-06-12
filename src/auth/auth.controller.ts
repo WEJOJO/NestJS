5fee8fee3ca6d41cc23a21ca83f1465e10d55323
Namik Hwang <nhwang@c8r1s2.42seoul.kr>
2023-06-12 16:12:28 +0900

    add_jwt_fin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {User} from "./user.entity";
+import { GetUser } from "./get-user.decorator";
 
 @Controller('auth')
 export class AuthController {
@@ -11,9 +14,18 @@ export class AuthController {
     {
         return this.authService.signUp(authCredentialDto);
     }
+
+
     @Post('/signin')
-    signIn(@Body(ValidationPipe) authCredentialDto : AuthCredentialDto) //: Promise<string>
+    signIn(@Body(ValidationPipe) authCredentialDto : AuthCredentialDto) : Promise<{accessToken:string}>
     {
         return this.authService.signIn(authCredentialDto);
     }
+
+    @Post('/test')
+    @UseGuards(AuthGuard())
+    test(@GetUser() user : User){
+        console.log('req',user);
+    }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         // @InjectRepository(UserRepository)
         private userRepository: UserRepository,
+        private jwtService: JwtService,
     ) {}
 
     //async 
@@ -17,7 +19,7 @@ export class AuthService {
         return this.userRepository.createUser(authCredentialDto);
     }
 
-    async signIn(authCredentialDto : AuthCredentialDto) : Promise<string>
+    async signIn(authCredentialDto : AuthCredentialDto) : Promise<{accessToken: string}>
     {
         const {username , password} = authCredentialDto;
         const user = await this.userRepository.findOne({
@@ -28,7 +30,10 @@ export class AuthService {
 
         if (user && (await bcrypt.compare(password, user.password)))
         {
-            return 'login success';
+            //user token create (secret + payload)
+            const payload = {username};
+            const accessToken = await this.jwtService.sign(payload);
+            return { accessToken }; // return obj
         }
         else
         {
